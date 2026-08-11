df06aa088377556e9034f8fbcb657cadbdcec005
Suman Kumar <sumankumarmks.cse@shivalikcollege.edu.in>
2026-08-11 11:15:42 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>>mpp;
+        vector<vector<string>>ans;
+
+        for( string  s :strs){
+            string key = s;
+            sort(key.begin(), key.end());
+            mpp[key].push_back(s);
+        }
+
+        for(auto &entry:mpp){
+            ans.push_back(entry.second);
+        }
+        return ans;
+    }
+};
